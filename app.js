7a44d6a9150a2c05806ca83c1b4d152e4efb50e6
John Roche <roche.johnjr@gmail.com>
2017-07-30 11:59:29 -0400

    added task object

--- app.js
+++ app.js
@@ -3,12 +3,27 @@ const mustacheExpress = require('mustache-express');
 
 const app = express();
 
+app.use(express.static('public'));
+
+// create data
+
+let todos = {
+  completed: [
+    { id: 0, task: "Drink coffee" },
+    { id: 1, task: "Read the news" },
+    { id: 2, task: "Get some exercise"}
+  ],
+  pending: [
+    { id: 3, task: "Work on TIY stuff" },
+    { id: 4, task: "Dinner with sis @ 7p" }
+  ]
+}
+
 app.engine('mst', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mst');
 
 app.get('/', (request,response) => {
-  // response.send('John we are doing it')
   response.render('home');
 });
 
