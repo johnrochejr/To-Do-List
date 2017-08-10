606fecbfa566ba4e1b1cb4f25d3e6d3943b05eb1
John Roche <roche.johnjr@gmail.com>
2017-08-10 08:43:07 -0400

    added sessions package

--- app.js
+++ app.js
@@ -2,31 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
+const expressSession = require('express-session');
 
 const app = express();
 
 app.use(express.static('public'));
-
-let todos2 = {
-  todos2: [
-    { id: 0, item: 'Drink coffee', status: 'completed' },
-    { id: 1, item: 'Read the news', status: 'pending' },
-    { id: 2, item: 'Get some exercise', status: 'pending' },
-    { id: 3, item: 'Work on TIY stuff', status: 'pending'}
-  ]
-};
-
-let todos = {
-  completed: [
-    'Drink coffee',
-    'Read the news',
-    'Get some exercise'
-  ],
-  pending: [
-    'Work on TIY stuff',
-    'Dinner with sis @ 7p'
-  ]
-}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({extended: false}));
@@ -35,14 +15,38 @@ app.engine('mst', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mst');
 
+app.use(expressValidator());
+
+app.use(
+  expressSession({
+    secret: 'tame impala',
+    resave: false,
+    saveUnitialized: true
+  })
+);
+
+// let todos = {
+//   todos: [
+//     { id: 0, item: 'Drink coffee', status: 'completed' },
+//     { id: 1, item: 'Read the news', status: 'pending' },
+//     { id: 2, item: 'Get some exercise', status: 'pending' },
+//     { id: 3, item: 'Work on TIY stuff', status: 'pending'}
+//   ]
+// };
 
 app.get('/', (request,response) => {
+  const todoList = request.session.todoList || [];
+
+  const todos = {
+    pending: todoList.filter(todo => !todo.completed),
+    completed: todoList.filter(todo => todo.completed)
+  };
   response.render('home', todos);
 });
 
-app.get('/todos2', (request, response) => {
-  let finishedTasks = todos2.todos2.filter(todo => todo.status === 'completed');
-  let unfinishedTasks = todos2.todos2.filter(todo => todo.status === 'pending')
+app.get('/todos', (request, response) => {
+  let finishedTasks = todos.todos.filter(todo => todo.status === 'completed');
+  let unfinishedTasks = todos.todos.filter(todo => todo.status === 'pending')
 
   let completionList = {
     completed: [],
@@ -62,21 +66,21 @@ app.get('/todos2', (request, response) => {
 app.post('/newTodo', (request, response) => {
 
   let newTask = request.body.todo;
-  let id = todos2.todos2.length;
-  todos2.todos2[id] = { id: id, item: newTask, status: 'pending' };
-  response.redirect('/todos2');
+  let id = todos.todos.length;
+  todos.todos[id] = { id: id, item: newTask, status: 'pending' };
+  response.redirect('/todos');
 });
 
 app.post('/completedTasks/:id', (request, response) => {
   let id = request.params.id;
-  todos2.todos2[id].status = 'completed';
-  response.redirect('/todos2');
+  todos.todos[id].status = 'completed';
+  response.redirect('/todos');
 });
 
 app.post('/notDone/:id', (request, response) => {
   let id = request.params.id;
-  todos2.todos2[id].status = 'pending'
-  response.redirect('/todos2');
+  todos.todos[id].status = 'pending'
+  response.redirect('/todos');
 });
 
 app.listen(3000, () => {
